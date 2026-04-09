89a2b825fadf25dd52e1ec3ede24e4dff4a5e843
Akujiezec <Akujiezec@coventry.ac.uk>
2026-04-09 01:52:56 +0100

    Update test_mqtt.py

--- tests/test_mqtt.py
+++ tests/test_mqtt.py
@@ -1,1 +1,21 @@
+import paho.mqtt.client as mqtt
+import json
 
+BROKER = "localhost"
+TOPIC = "ambulance/patient"
+
+def on_connect(client, userdata, flags, rc):
+    print("Test client connected." if rc == 0 else f"Failed: {rc}")
+    client.subscribe(TOPIC)
+
+def on_message(client, userdata, msg):
+    data = json.loads(msg.payload.decode())
+    print("[TEST] Message received:")
+    print(data)
+    client.disconnect()
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(BROKER, 1883, 60)
+client.loop_forever()
