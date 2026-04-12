f6a58acc2ef9f3e1b7a42f915b287ebade0f8899
ujamk <ujamk@coventry.ac.uk>
2026-04-12 17:34:28 +0100

    add sqlite database setup

--- src/login_system.py
+++ src/login_system.py
@@ -7,3 +7,27 @@ DB_NAME = "smart_ambulance_users.db"
 
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
+
+def create_database():
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL,
+            role TEXT NOT NULL
+        )
+    """)
+
+    conn.commit()
+    conn.close()# Smart Ambulance Login System
+
+import sqlite3
+import hashlib
+
+DB_NAME = "smart_ambulance_users.db"
+
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
