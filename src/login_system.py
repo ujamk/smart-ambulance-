a16d97b6b8474a01347303ad6e4f708a4a5f0300
ujamk <ujamk@coventry.ac.uk>
2026-04-12 17:31:09 +0100

    add login system initial structure

---
+++ src/login_system.py
@@ -0,0 +1,9 @@
+# Smart Ambulance Login System
+
+import sqlite3
+import hashlib
+
+DB_NAME = "smart_ambulance_users.db"
+
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
